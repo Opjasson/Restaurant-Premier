13d26ca27c4ce70e4dcfe57df7e064989c692d49
Opjasson <azmitheusos333@gmail.com>
2025-04-21 06:37:06 +0700

    frontend : menambahkan AddStock page

---
+++ restoPremier/src/Pages/AddStock.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../Components/Templates/MainLayout'
+
+const AddStock = () => {
+  return (
+    <MainLayout>
+      <h1>Input data</h1>
+    </MainLayout>
+  )
+}
+
+export default AddStock
