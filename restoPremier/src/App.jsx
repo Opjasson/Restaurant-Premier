a6794ab2d461b32936c2094ae9bdff946db3f52d
Opjasson <azmitheusos333@gmail.com>
2025-04-21 19:55:18 +0700

    frontend : done sideba dropdown menu style

--- restoPremier/src/App.jsx
+++ restoPremier/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Stock from "./Pages/Stock";
--- restoPremier/src/Components/Organism/Navbar.jsx
+++ restoPremier/src/Components/Organism/Navbar.jsx
@@ -1,20 +1,105 @@
-import React from 'react'
-import { premier } from '../../assets'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { premier } from "../../assets";
+import { Link } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
-  return (
-      <nav className="flex items-center justify-between px-8 w-full">
-          <div className="flex items-center">
-              <img src={premier} alt="" className="lg:h-24 h-16" />
-              <h1 className="lg:text-xl text-base italic">Stock Resto</h1>
-          </div>
-          <div className="lg:w-36 w-24 flex justify-between">
-              <Link className="lg:text-xl text-sm" to={"/"}>HOME</Link>
-              <Link className="lg:text-xl text-sm" to={"/stock"}>STOCK</Link>
-          </div>
-      </nav>
-  );
-}
+    const handleDropDown = () => {
+        const getElement = document.querySelector("#dropDown");
+        switch (getElement.hasAttribute('hidden')) {
+            case true:
+                getElement.removeAttribute("hidden");
+                break;
+            case false:
+                getElement.setAttribute("hidden","");
+                break;
+            default:
+                break;
+        }
+        
+        
+    };
 
-export default Navbar
+    return (
+        <nav className="flex items-center justify-between px-8 w-full">
+            <div className="flex items-center">
+                <img src={premier} alt="" className="lg:h-24 h-16" />
+                <h1 className="lg:text-xl text-base italic">Stock Resto</h1>
+            </div>
+            <div className="w-1/2 lg:flex hidden justify-between">
+                <Link
+                    className="lg:text-lg text-sm hover:text-slate-500 hover:underline"
+                    to={"/"}>
+                    HOME
+                </Link>
+                <Link
+                    className="lg:text-lg text-sm hover:text-slate-500 hover:underline"
+                    to={"/stock"}>
+                    STOCK AWAL
+                </Link>
+                <Link
+                    className="lg:text-lg text-sm hover:text-slate-500 hover:underline"
+                    to={"/stock"}>
+                    BARANG MASUK
+                </Link>
+                <Link
+                    className="lg:text-lg text-sm hover:text-slate-500 hover:underline"
+                    to={"/stock"}>
+                    BARANG KELUAR
+                </Link>
+                <Link
+                    className="lg:text-lg text-sm hover:text-slate-500 hover:underline"
+                    to={"/stock"}>
+                    STOCK AKHIR
+                </Link>
+            </div>
+
+            <IoMenu onClick={() => handleDropDown()} className="lg:hidden block text-xl hover:cursor-pointer" />
+
+            {/* Start DropDown */}
+            <div
+                hidden
+                id="dropDown"
+                className="bg-slate-600 p-5 rounded-bl-lg rounded-tl-lg absolute top-16 right-0 h-2/3 w-60 list-none ">
+                <li className="mb-10">
+                    <Link
+                        className="text-lg hover:text-slate-500 hover:underline text-white"
+                        to={"/"}>
+                        HOME
+                    </Link>
+                </li>
+                <li className="mb-10">
+                    <Link
+                        className="text-lg hover:text-slate-500 hover:underline text-white"
+                        to={"/stock"}>
+                        STOCK AWAL
+                    </Link>
+                </li>
+                <li className="mb-10">
+                    <Link
+                        className="text-lg hover:text-slate-500 hover:underline text-white"
+                        to={"/stock"}>
+                        BARANG MASUK
+                    </Link>
+                </li>
+                <li className="mb-10">
+                    <Link
+                        className="text-lg hover:text-slate-500 hover:underline text-white"
+                        to={"/stock"}>
+                        BARANG KELUAR
+                    </Link>
+                </li>
+                <li className="mb-10">
+                    <Link
+                        className="text-lg hover:text-slate-500 hover:underline text-white"
+                        to={"/stock"}>
+                        STOCK AKHIR
+                    </Link>
+                </li>
+            </div>
+            {/* End Drop Down */}
+        </nav>
+    );
+};
+
+export default Navbar;
