5243eeb7c7287f45d4fc4bd984eeca0d526be177
Opjasson <azmitheusos333@gmail.com>
2025-04-24 22:30:38 +0700

    frontend : done setting conect detail stock page. to update stock

--- restoPremier/src/Pages/DetailStock.jsx
+++ restoPremier/src/Pages/DetailStock.jsx
@@ -1,90 +1,124 @@
-import React, { useState } from 'react'
-import MainLayout from '../Components/Templates/MainLayout'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import MainLayout from "../Components/Templates/MainLayout";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const DetailStock = () => {
-  const [data, setData] = useState([])
+    const [data, setData] = useState({});
+    const { id } = useParams();
 
-  const getData = async () => {
-    const response = await axios.get("")
-  }
+    const getData = async () => {
+        try {
+            const stock = await axios.get(`http://localhost:8000/stock/${id}`);
+            setData(stock.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  return (
-      <MainLayout>
-          <div className="mb-10 bg-blue-500 md:w-1/2 p-3 rounded-br-4xl rounded-sm text-white">
-              <h1 className="md:text-4xl text-2xl font-extrabold">
-                  Detail stock
-              </h1>
-              <p className="md:text-xl font-light">
-                  Informasi lengkap stock
-              </p>
-          </div>
+    useEffect(() => {
+        getData()
+    }, {})
+    // console.log(data);
+    
 
-          {/* Form Start */}
-          <form
-              action=""
-              className="flex flex-col gap-5 md:w-2/3 mx-auto pb-20">
-              <div className="flex flex-col md:gap-2">
-                  <label htmlFor="namaStock" className="md:text-xl text-base">
-                      Nama Stock
-                  </label>
-                  <select
-                      name=""
-                      id="namaStock"
-                      className="border md:p-2 p-1.5 rounded-xl">
-                      <option value="">Tepung Terigu</option>
-                      <option value="">Kecap Bango</option>
-                      <option value="">Beras</option>
-                      <option value="">Tepung Kanji</option>
-                      <option value="">Tepung Beras</option>
-                      <option value="">Ayam</option>
-                      <option value="">Telor</option>
-                      <option value="">Tempe</option>
-                      <option value="">Tahu</option>
-                  </select>
-              </div>
+    return (
+        <MainLayout>
+            <div className="mb-10 bg-blue-500 md:w-1/2 p-3 rounded-br-4xl rounded-sm text-white">
+                <h1 className="md:text-4xl text-2xl font-extrabold">
+                    Detail stock
+                </h1>
+                <p className="md:text-xl font-light">Informasi lengkap stock</p>
+            </div>
 
-              <div className="flex flex-col md:gap-2">
-                  <label className="md:text-xl text-base" htmlFor="namaStock">
-                      Jumlah
-                  </label>
-                  <input
-                      id="namaStock"
-                      className="border rounded-xl p-1.5 md:p-2"
-                      type="number"
-                  />
-              </div>
+            {/* Form Start */}
+            <form
+                action=""
+                className="flex flex-col gap-5 md:w-2/3 mx-auto pb-20">
+                <div className="flex flex-col md:gap-2">
+                    <label htmlFor="namaStock" className="md:text-xl text-base">
+                        Nama Stock
+                    </label>
+                    <select
+                        name=""
+                        id="namaStock"
+                        className="border md:p-2 p-1.5 rounded-xl">
+                        <option value={data.nama_Barang}>
+                            {data.nama_Barang}
+                        </option>
+                        <option value="">Tepung Terigu</option>
+                        <option value="">Kecap Bango</option>
+                        <option value="">Beras</option>
+                        <option value="">Tepung Kanji</option>
+                        <option value="">Tepung Beras</option>
+                        <option value="">Ayam</option>
+                        <option value="">Telor</option>
+                        <option value="">Tempe</option>
+                        <option value="">Tahu</option>
+                    </select>
+                </div>
 
-              <div className="flex flex-col md:gap-2">
-                  <label className="md:text-xl text-base" htmlFor="catatan">
-                      Catatan
-                  </label>
-                  <textarea
-                      name=""
-                      className="border rounded-xl p-1.5 md:p-2"
-                      id="catatan"></textarea>
-              </div>
+                <div className="flex flex-col md:gap-2">
+                    <label className="md:text-xl text-base" htmlFor="namaStock">
+                        Stok awal
+                    </label>
+                    <input
+                        id="namaStock"
+                        className="border rounded-xl p-1.5 md:p-2"
+                        type="number"
+                        value={data.stok_awal}
+                    />
+                </div>
 
-              <div className="flex flex-col md:gap-2">
-                  <label className="md:text-xl text-base" htmlFor="namaStock">
-                      Tanggal Masuk
-                  </label>
-                  <input
-                      id="namaStock"
-                      className="border rounded-xl p-1.5 md:p-2"
-                      type="datetime-local"
-                  />
-              </div>
+                <div className="flex flex-col md:gap-2">
+                    <label
+                        className="md:text-xl text-base"
+                        htmlFor="barangMasuk">
+                        Barang masuk
+                    </label>
+                    <input
+                        id="barangMasuk"
+                        className="border rounded-xl p-1.5 md:p-2"
+                        type="number"
+                        value={data.barang_masuk}
+                    />
+                </div>
 
-              <button
-                  type="submit"
-                  className="bg-blue-500 w-1/4 mx-auto px-3 py-2 hover:cursor-pointer hover:bg-blue-600 rounded-xl text-white font-extrabold">
-                  Buat
-              </button>
-          </form>
-          {/* Form End */}
-      </MainLayout>
-  );
-}
+                <div className="flex flex-col md:gap-2">
+                    <label
+                        className="md:text-xl text-base"
+                        htmlFor="barangKeluar">
+                        Barang keluar
+                    </label>
+                    <input
+                        id="barangKeluar"
+                        className="border rounded-xl p-1.5 md:p-2"
+                        type="number"
+                        value={data.barang_keluar}
+                    />
+                </div>
 
-export default DetailStock
+                <div className="flex flex-col md:gap-2">
+                    <label className="md:text-xl text-base" htmlFor="stokAkhir">
+                        Stok akhir
+                    </label>
+                    <input
+                        id="stokAkhir"
+                        className="border rounded-xl p-1.5 md:p-2"
+                        type="number"
+                        value={data.stok_akhir}
+                    />
+                </div>
+
+                <button
+                    type="submit"
+                    className="bg-blue-500 w-1/4 mx-auto px-3 py-2 hover:cursor-pointer hover:bg-blue-600 rounded-xl text-white font-extrabold">
+                    Buat
+                </button>
+            </form>
+            {/* Form End */}
+        </MainLayout>
+    );
+};
+
+export default DetailStock;
--- restoPremier/src/Pages/Stock.jsx
+++ restoPremier/src/Pages/Stock.jsx
@@ -59,7 +59,7 @@ const Stock = () => {
                     <div
                         onClick={() => navigate(`Detail-stock/${item.id}`)}
                         className="flex justify-between hover:cursor-pointer px-5 py-3 lg:text-lg text-sm font-extralight mt-2 border-b-2 border-slate-400">
-                        <h2 className="">{index + 1}</h2>
+                        <h2 key={index + 1} className="">{index + 1}</h2>
                         <h2 className=" w-32 ml-3">{item.nama_Barang}</h2>
                         <h2 className=" w-32">{item.stok_awal}</h2>
                         <h2 className=" w-32">{item.barang_masuk}</h2>
