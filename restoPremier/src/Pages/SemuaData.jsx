773d30f11397331660243eb8c5fb6d94beb40b24
Opjasson <azmitheusos333@gmail.com>
2025-05-02 09:06:17 +0700

    frontend : done solved filter data based date input in SemuaData page

--- restoPremier/src/Pages/SemuaData.jsx
+++ restoPremier/src/Pages/SemuaData.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
 import MainLayout from "../Components/Templates/MainLayout";
 import axios from "axios";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import _ from "lodash";
 
 const SemuaData = () => {
     const [data, setData] = useState([]);
-    const [findLower, setfindLower] = useState("");
+    const [findLower, setfindLower] = useState("null");
 
     const navigate = useNavigate();
 
@@ -38,19 +38,21 @@ const SemuaData = () => {
     });
 
     // set up print
-
     const handlePrint = () => {
-        const Navbar = document.querySelector('nav')
-        const HeadPage = document.querySelector('#headPage')
-        const PrintButton = document.querySelector('#printButton')
-        const TombolKembali = document.querySelector('#tombolKembali')
+        const Navbar = document.querySelector("nav");
+        const HeadPage = document.querySelector("#headPage");
+        const PrintButton = document.querySelector("#printButton");
+        const TombolKembali = document.querySelector("#tombolKembali");
 
-        Navbar.setAttribute("hidden", "")
-        HeadPage.setAttribute("hidden", "")
-        PrintButton.setAttribute("hidden", "")
-        window.print()
-        TombolKembali.removeAttribute("hidden")
+        Navbar.setAttribute("hidden", "");
+        HeadPage.setAttribute("hidden", "");
+        PrintButton.setAttribute("hidden", "");
+        window.print();
+        TombolKembali.removeAttribute("hidden");
     };
+    // end set up print
+
+    console.log(findLower);
 
     return (
         <MainLayout>
@@ -73,61 +75,7 @@ const SemuaData = () => {
                 </div>
             </div>
 
-            {groupData[findLower] ? (
-                <div
-                    className="mt-7 border-b-2 border-blue-500 pb-16"
-                    id="mainContent">
-
-                    <button
-                        id="printButton"
-                        onClick={handlePrint}
-                        className="hover:cursor-pointer hover:underline">
-                        Print
-                    </button>
-
-                    <button
-                    hidden
-                        id="tombolKembali"
-                        onClick={() => navigate("/")}
-                        className="hover:cursor-pointer flex hover:underline">
-                        <svg width="20" height="20" className="rotate-180">
-                            <path d="M10 0 L20 10 L10 20 Z" fill="#000" />
-                        </svg>
-                        Kembali
-                    </button>
-
-                    <p>{findLower}</p>
-                    <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
-                        <h2>No</h2>
-                        <h2 className="lg:ml-0 ml-1.5 lg:w-40">Nama barang</h2>
-                        <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">
-                            Satuan
-                        </h2>
-                        <h2 className=" lg:w-32 w-fit">Stock awal</h2>
-                        <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
-                        <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
-                        <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
-                    </div>
-                    {groupData[findLower]?.map((test, index) => (
-                        <div
-                            key={test.id}
-                            onClick={() => navigate(`Detail-stock/${test.id}`)}
-                            className="flex justify-between hover:cursor-pointer hover:bg-slate-300 lg:px-5 px-2 py-3 lg:text-lg text-sm font-extralight mt-2 border-b-2 border-slate-400 border">
-                            <h2 key={index + 1} className="">
-                                {index + 1}
-                            </h2>
-                            <h2 className=" w-40 ml-3 capitalize">
-                                {test.nama_Barang}
-                            </h2>
-                            <h2 className="w-32">{test.satuan}</h2>
-                            <h2 className="w-32">{test.stok_awal}</h2>
-                            <h2 className="w-32">{test.barang_masuk}</h2>
-                            <h2 className="w-32">{test.barang_keluar}</h2>
-                            <h2 className="w-32">{test.stok_akhir}</h2>
-                        </div>
-                    ))}
-                </div>
-            ) : (
+            {findLower === "null" ? (
                 filterGroping.map((key, index) => (
                     <div
                         className="mt-7 border-b-2 border-blue-500 pb-16"
@@ -175,6 +123,76 @@ const SemuaData = () => {
                         ]}
                     </div>
                 ))
+            ) : groupData[findLower] ? (
+                <div
+                    className="mt-7 border-b-2 border-blue-500 pb-16"
+                    id="mainContent">
+                    <button
+                        id="printButton"
+                        onClick={handlePrint}
+                        className="hover:cursor-pointer hover:underline">
+                        Print
+                    </button>
+
+                    <button
+                        hidden
+                        id="tombolKembali"
+                        onClick={() => navigate("/")}
+                        className="hover:cursor-pointer flex hover:underline">
+                        <svg width="20" height="20" className="rotate-180">
+                            <path d="M10 0 L20 10 L10 20 Z" fill="#000" />
+                        </svg>
+                        Kembali
+                    </button>
+
+                    <p>{findLower}</p>
+                    <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
+                        <h2>No</h2>
+                        <h2 className="lg:ml-0 ml-1.5 lg:w-40">Nama barang</h2>
+                        <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">
+                            Satuan
+                        </h2>
+                        <h2 className=" lg:w-32 w-fit">Stock awal</h2>
+                        <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
+                        <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
+                        <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
+                    </div>
+                    {groupData[findLower]?.map((test, index) => (
+                        <div
+                            key={test.id}
+                            onClick={() => navigate(`Detail-stock/${test.id}`)}
+                            className="flex justify-between hover:cursor-pointer hover:bg-slate-300 lg:px-5 px-2 py-3 lg:text-lg text-sm font-extralight mt-2 border-b-2 border-slate-400 border">
+                            <h2 key={index + 1} className="">
+                                {index + 1}
+                            </h2>
+                            <h2 className=" w-40 ml-3 capitalize">
+                                {test.nama_Barang}
+                            </h2>
+                            <h2 className="w-32">{test.satuan}</h2>
+                            <h2 className="w-32">{test.stok_awal}</h2>
+                            <h2 className="w-32">{test.barang_masuk}</h2>
+                            <h2 className="w-32">{test.barang_keluar}</h2>
+                            <h2 className="w-32">{test.stok_akhir}</h2>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div>
+                    <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
+                        <h2>No</h2>
+                        <h2 className="lg:ml-0 ml-1.5 lg:w-40">Nama barang</h2>
+                        <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">
+                            Satuan
+                        </h2>
+                        <h2 className=" lg:w-32 w-fit">Stock awal</h2>
+                        <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
+                        <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
+                        <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
+                    </div>
+                    <p className="p-7">
+                        Data kosong pada tanggal yang anda masukan!
+                    </p>
+                </div>
             )}
         </MainLayout>
     );
