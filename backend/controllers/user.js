97169daccb7d2f86bfff068c4694101377012502
Opjasson <azmitheusos333@gmail.com>
2025-06-20 19:10:53 +0700

    backend : add role attribute in controllers user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,7 +4,7 @@ import argon2 from "argon2";
 export const getUsers = async (req, res) => {
     try {
         const response = await Users.findAll({
-            attributes: ["id", "email"],
+            attributes: ["id", "email", "role"],
         });
         res.status(200).json({ msg: "get data succesfully", data: response });
     } catch (error) {
@@ -26,7 +26,7 @@ export const getUserById = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
-    const { email, password, confPassword } = req.body;
+    const { email, role, password, confPassword } = req.body;
 
     if (password !== confPassword) {
         return res
@@ -38,6 +38,7 @@ export const createUser = async (req, res) => {
     try {
         await Users.create({
             email,
+            role,
             password: HashPassword,
         });
         res.status(201).json({ msg: "Register berhasil" });
@@ -47,7 +48,7 @@ export const createUser = async (req, res) => {
 };
 
 export const updateUsersById = async (req, res) => {
-    const { email, password, confPassword } = req.body;
+    const { email, role ,password, confPassword } = req.body;
 
     const getUserById = await Users.findOne({
         where: {
@@ -66,6 +67,7 @@ export const updateUsersById = async (req, res) => {
         await Users.update(
             {
                 email,
+                role,
                 password: HashPassword,
             },
             {
