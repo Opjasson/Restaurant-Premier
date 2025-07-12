f117f3028dd78c31aceec2d99e57dd626ad10e06
Opjasson <azmitheusos333@gmail.com>
2025-07-12 18:36:08 +0700

    frontend : setting filter data range date input

--- restoPremier/src/Pages/SemuaData.jsx
+++ restoPremier/src/Pages/SemuaData.jsx
@@ -7,7 +7,8 @@ import _ from "lodash";
 const SemuaData = () => {
     // useState adalah tempat penyimpanan sementara
     const [data, setData] = useState([]);
-    const [findLower, setfindLower] = useState("null");
+    const [findLower1, setfindLower1] = useState("null");
+    const [findLower2, setfindLower2] = useState("null");
     // ------
 
     // navigate untuk berpindah ke halaman lainnya
@@ -54,6 +55,15 @@ const SemuaData = () => {
     });
     // --------
 
+    // Range tanggal yang dipilih
+    const startDate = new Date(findLower1.split("T")[0]);
+    const endDate = new Date(findLower2.split("T")[0]);
+
+    const filteredData = data.filter((a) => {
+        const tgl = new Date(a.createdAt.split("T")[0]);
+        return tgl >= startDate && tgl <= endDate;
+    });
+
     // set up print
     const handlePrint = () => {
         const Navbar = document.querySelector("nav");
@@ -83,143 +93,69 @@ const SemuaData = () => {
                     </p>
                 </div>
 
-                <div className="flex items-center bg-slate-300 lg:px-2 px-1 lg:py-1.5 py-0 lg:rounded-xl rounded-sm lg:w-80 w-[55%] h-fit">
-                    <input
-                        type="date"
-                        className="outline-none w-full text-sm"
-                        onChange={(e) => setfindLower(e.target.value)}
-                    />
+                <div className="flex items-center border w-[50%] bg-slate-300 lg:px-2 px-1 lg:py-1.5 py-0 lg:rounded-xl rounded-sm h-fit justify-between">
+                    <div>
+                        <p>dari tanggal :</p>
+                        <input
+                            type="date"
+                            className="outline-none w-52 border text-sm"
+                            onChange={(e) => setfindLower1(e.target.value)}
+                        />
+                    </div>
+                    <p className="w-32 border"></p>
+
+                    <div>
+                        <p>sampai tanggal :</p>
+                        <input
+                            type="date"
+                            className="outline-none w-52 border text-sm"
+                            onChange={(e) => setfindLower2(e.target.value)}
+                        />
+                    </div>
                 </div>
             </div>
             {/* end head judul halaman */}
 
             {/* menampilkan data atau list data */}
-            {findLower === "null" ? (
-                filterGroping.map((key, index) => (
-                    <div
-                        className="mt-7 border-b-2 border-blue-500 pb-16"
-                        key={index}>
-                        {/* menampilkan semua data jika belum memasukan tanggal */}
-                        <p>{key}</p>
-                        <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
-                            <h2>No</h2>
-                            <h2 className="lg:ml-0 ml-1.5 lg:w-40">
-                                Nama barang
-                            </h2>
-                            <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">
-                                Satuan
-                            </h2>
-                            <h2 className=" lg:w-32 w-fit">Stock awal</h2>
-                            <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
-                            <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
-                            <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
-                        </div>
 
-                        {[
-                            Object.values(groupData[key]).map((item, index) => (
-                                <div
-                                    key={index}
-                                    className="flex justify-between hover:cursor-pointer hover:bg-slate-300 lg:px-5 px-2 py-3 lg:text-lg text-sm font-extralight mt-2 border-b-2 border-slate-400 border">
-                                    <h2 key={index + 1} className="">
-                                        {index + 1}
-                                    </h2>
-                                    <h2 className=" w-40 ml-3 capitalize">
-                                        {item.nama_Barang}
-                                    </h2>
-                                    <h2 className="w-32">{item.satuan}</h2>
-                                    <h2 className="w-32">{item.stok_awal}</h2>
-                                    <h2 className="w-32">
-                                        {item.barang_masuk}
-                                    </h2>
-                                    <h2 className="w-32">
-                                        {item.barang_keluar}
-                                    </h2>
-                                    <h2 className="w-32">{item.stok_akhir}</h2>
-                                </div>
-                            )),
-                        ]}
-                        {/* ---------- */}
-                    </div>
-                ))
-            ) : groupData[findLower] ? (
-                <div
-                    className="mt-7 border-b-2 border-blue-500 pb-16"
-                    id="mainContent">
-                    {/* menampilkan data berdasarkan tanggal yang dimasukan */}
+            <div className="mt-7 border-b-2 border-blue-500 pb-16">
+                {/* menampilkan semua data jika belum memasukan tanggal */}
+                <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
+                    <h2>No</h2>
+                    <h2 className="lg:ml-0 ml-1.5 lg:w-40">Nama barang</h2>
+                    <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">Satuan</h2>
+                    <h2 className=" lg:w-32 w-fit">Stock awal</h2>
+                    <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
+                    <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
+                    <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
+                </div>
 
-                    <button
-                        id="printButton"
-                        onClick={handlePrint}
-                        className="hover:cursor-pointer hover:underline bg-green-500 px-2.5 rounded-lg">
-                        Print
-                    </button>
-
-                    <button
-                        hidden
-                        id="tombolKembali"
-                        onClick={() => navigate("/")}
-                        className="hover:cursor-pointer flex hover:underline">
-                        <svg width="20" height="20" className="rotate-180">
-                            <path d="M10 0 L20 10 L10 20 Z" fill="#000" />
-                        </svg>
-                        Kembali
-                    </button>
-
-                    <div className="border border-blue-400">
-                        <h1 className="text-center font-bold text-2xl">LAPORAN DATA STOK BAHAN BAKU RESTO PREMIER HOTEL TEGAL</h1>
-                    </div>
-                    <p>{findLower}</p>
-                    <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
-                        <h2>No</h2>
-                        <h2 className="lg:ml-0 ml-1.5 lg:w-40">Nama barang</h2>
-                        <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">
-                            Satuan
-                        </h2>
-                        <h2 className=" lg:w-32 w-fit">Stock awal</h2>
-                        <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
-                        <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
-                        <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
-                    </div>
-                    {groupData[findLower]?.map((test, index) => (
+                {filteredData.length > 0 ? (
+                    filteredData.map((item, index) => (
                         <div
-                            key={test.id}
-                            onClick={() => navigate(`Detail-stock/${test.id}`)}
+                            key={index}
                             className="flex justify-between hover:cursor-pointer hover:bg-slate-300 lg:px-5 px-2 py-3 lg:text-lg text-sm font-extralight mt-2 border-b-2 border-slate-400 border">
                             <h2 key={index + 1} className="">
                                 {index + 1}
                             </h2>
                             <h2 className=" w-40 ml-3 capitalize">
-                                {test.nama_Barang}
+                                {item.nama_Barang}
                             </h2>
-                            <h2 className="w-32">{test.satuan}</h2>
-                            <h2 className="w-32">{test.stok_awal}</h2>
-                            <h2 className="w-32">{test.barang_masuk}</h2>
-                            <h2 className="w-32">{test.barang_keluar}</h2>
-                            <h2 className="w-32">{test.stok_akhir}</h2>
+                            <h2 className="w-32">{item.satuan}</h2>
+                            <h2 className="w-32">{item.stok_awal}</h2>
+                            <h2 className="w-32">{item.barang_masuk}</h2>
+                            <h2 className="w-32">{item.barang_keluar}</h2>
+                            <h2 className="w-32">{item.stok_akhir}</h2>
                         </div>
-                    ))}
-
-                    {/* -------------- */}
-                </div>
-            ) : (
-                <div>
-                    {/*jika data pada tanggal yang dimasukan tidak ada */}
-                    <div className="flex justify-between px-5 py-3 bg-blue-500 rounded-xl lg:text-lg text-[12px] text-white font-bold shadow-slate-500 shadow-md">
-                        <h2>No</h2>
-                        <h2 className="lg:ml-0 ml-1.5 lg:w-40">Nama barang</h2>
-                        <h2 className=" lg:w-32 w-fit lg:ml-0 mr-1.5">
-                            Satuan
-                        </h2>
-                        <h2 className=" lg:w-32 w-fit">Stock awal</h2>
-                        <h2 className=" lg:w-32 w-fit">Barang masuk</h2>
-                        <h2 className=" lg:w-32 w-fit">Barang keluar</h2>
-                        <h2 className=" lg:w-32 w-fit">Stock akhir</h2>
-                    </div>
+                    ))
+                ) : (
                     <p className="p-7">
                         Data kosong pada tanggal yang anda masukan!
                     </p>
-                </div>
-            )}
+                )}
+
+                {/* ---------- */}
+            </div>
             {/* end menampilkan list data */}
         </MainLayout>
         // end tampilan halaman data
